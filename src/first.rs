bb9436029a18b6a12239696b126653eb17c9bed9
irayspace <irayspacii@gmail.com>
2022-12-13 22:35:34 +0800

    feat: calorie counting part two

--- src/first.rs
+++ src/first.rs
@@ -20,3 +20,37 @@ pub fn execute() {
     }
     println!("{:?}", elf_most_calories);
 }
+
+
+pub fn execute_two() {
+    let data = utils::get_data(1);
+    let split_data = data.split("\n");
+    let mut elf_calories = 0;
+    let mut elf_first_most_calories = 0;
+    let mut elf_second_most_calories = 0;
+    let mut elf_third_most_calories = 0;
+    for s in split_data {
+        match s.parse::<i32>() {
+            Ok(n) => {
+                elf_calories = elf_calories + n;
+            }
+            _ => {
+                if elf_third_most_calories < elf_calories {
+                    elf_third_most_calories = elf_calories;
+                }
+                if elf_second_most_calories < elf_third_most_calories {
+                    let temp = elf_second_most_calories;
+                    elf_second_most_calories = elf_third_most_calories;
+                    elf_third_most_calories = temp;
+                }
+                if elf_first_most_calories < elf_second_most_calories {
+                    let temp = elf_first_most_calories;
+                    elf_first_most_calories = elf_second_most_calories;
+                    elf_second_most_calories = temp;
+                }
+                elf_calories = 0;
+            }
+        }
+    }
+    println!("{:?}", elf_first_most_calories + elf_second_most_calories + elf_third_most_calories);
+}
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ mod utils;
 fn main() {
     // Get data from using utils::get_data(x)
     // with [x] as int parameter
-    first::execute();
+    first::execute_two();
 }
